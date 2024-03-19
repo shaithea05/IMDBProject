9c7780298d50af5910b138cd641665f49dfe2952
Ming-Shih <minstone@gmail.com>
2024-03-19 15:10:35 -0400

    complete queries 1-5

---
+++ db.php
@@ -0,0 +1,64 @@
+
+<?php
+// generic table builder. It will automatically build table data rows irrespective of result
+class TableRows extends RecursiveIteratorIterator
+{
+	function __construct($it)
+	{
+		parent::__construct($it, self::LEAVES_ONLY);
+	}
+
+	function current()
+	{
+		return "<td style='text-align:center'>" . parent::current() . "</td>";
+	}
+
+	function beginChildren()
+	{
+		echo "<tr>";
+	}
+
+	function endChildren()
+	{
+		echo "</tr>" . "\n";
+	}
+}
+
+// SQL CONNECTIONS
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "COSI127b";
+
+try {
+	// We will use PDO to connect to MySQL DB. This part need not be 
+	// replicated if we are having multiple queries. 
+	// initialize connection and set attributes for errors/exceptions
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	// prepare statement for executions. This part needs to change for every query
+	$stmt = $conn->prepare($query);
+
+	// execute statement
+	$stmt->execute();
+
+	// set the resulting array to associative. 
+	$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+	if ($stmt->rowCount() == 0) {
+		echo "<h4>No results found. Please try again.</h4>";
+	} else {	// for each row that we fetched, use the iterator to build a table row on front-end
+		foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
+			echo $v;
+		}
+	}
+
+} catch (PDOException $e) {
+	echo "Error: " . $e->getMessage();
+}
+echo "</table>";
+// destroy our connection
+$conn = null;
+
+?>
--- index.php
+++ index.php
@@ -1,171 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <!-- Bootstrap JS dependencies -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<!-- Bootstrap JS dependencies -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IMDB Movie Database</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>IMDB Movie Database</title>
 </head>
 <body>
-    <div class="container" style="margin-top: 20px;">
-        <h1 style="text-align:center">IMDB Movie Database</h1><br>
-    </div>
-    <div class="container">
-		<h3 style="text-align:left">Query Options<span style="font-size: 15px; margin-left: 10px;"><i>Example user for testing: alice.johnson@example.com</i></span></h3><br>
+	<div class="container" style="margin-top: 20px;">
+		<h1 style="text-align:center">IMDB Movie Database</h1><br>
+	</div>
+	<div class="container">
+		<h3 style="text-align:left">Query Options</h3><br>
 
-        <form id="ageLimitForm" method="post" action="index.php">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Enter user email" name="userEmail" id="userEmail">
-				<div class="input-group-append" >
-                    <button class="btn btn-outline-secondary" type="submit" name="userLikingMovies" id="button-addon2">User liking movies</button>
-                </div>
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="submit" name="viewAllMovies" id="button-addon2">View all movies</button>
-                </div>
+		<form id="ageLimitForm" method="post" action="index.php">
+
+			<!-- buttons without parameterized queries -->
+			<div class="input-group mb-3">
 				<div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="submit" name="viewAllActors" id="button-addon2">View all actors</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    <div class="container">	
-		<h3 style="text-align:left">Query Result</h3><br>
-		
-        <?php
-			// we will now create a table from PHP side 
-			echo "<table class='table table-md table-bordered'>";
-			echo "<thead class='thead-dark' style='text-align: center'>";
-	
-			if(isset($_POST['userLikingMovies'])){
-				$userEmail = $_POST["userEmail"]; 
+					<button class="btn btn-outline-secondary" type="submit" name="viewAllActors" id="button-addon2">View all actors</button>
+				</div>
+				<div class="input-group-append">
+					<button class="btn btn-outline-secondary" type="submit" name="viewAllTables" id="button-addon2">View all tables</button>
+				</div>
+				<div class="input-group-append">
+					<button class="btn btn-outline-secondary" type="submit" name="viewAllMovies" id="button-addon2">View all movies</button>
+				</div>
+			</div>
 
+			<div class="row">
+				<div class="col">
+					<h6>Filter movies liked by a user</h6>
+				</div>
+				<div class="col">
+					<h6>Search movie details</h6>
+				</div>
+			</div>
 
-				if (empty($userEmail)) {
-					$userEmail = "";
-					$query = "
-					SELECT User.email, User.name AS userName, MotionPicture.name
-					FROM User
-					JOIN Likes ON User.email = Likes.uemail
-					JOIN MotionPicture ON Likes.mpid = MotionPicture.id;
-					";
-				}else{
-					$query = "
-					SELECT User.email, User.name AS userName, MotionPicture.name
-					FROM User
-					JOIN Likes ON User.email = Likes.uemail
-					JOIN MotionPicture ON Likes.mpid = MotionPicture.id
-					WHERE Likes.uemail = '$userEmail';
-					";
-				}
+			<!-- buttons with parameterized queries -->
+			<div class="row">
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Enter user email" name="userEmail" id="userEmail">
+				</div>
+				<div class="col">
+					<button class="btn btn-outline-secondary" type="submit" name="userLikingMovies" id="button-addon2">View movies</button>
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Enter movie name" name="motionPicture" id="motionPicture">
+				</div>
+				<div class="col">
+					<button class="btn btn-outline-secondary" type="submit" name="viewMovies" id="button-addon2">View movie details</button>
+				</div>
+			</div>
+
+			<div style="padding-bottom: 20px;"></div>
+
+			<div class="row">
+				<div class="col">
+					<h6>Filter movies by shooting location</h6>
+				</div>
+				<div class="col">
+					<h6>Filter directors by shooting location zip</h6>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Enter country" name="shootingLocation" id="shootingLocation">
+				</div>
+				<div class="col">
+					<button class="btn btn-outline-secondary" type="submit" name="viewMoviesFromLocation" id="button-addon2">View Movies</button>
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Enter zip code" name="zipCode" id="zipCode">
+				</div>
+				<div class="col">
+					<button class="btn btn-outline-secondary" type="submit" name="viewDirectorsFromZipCode" id="button-addon2">View directors</button>
+				</div>
+			</div>
+
+			<div style="padding-bottom: 20px;"></div>
 			
+			<div class="row">
+				<div class="col">
+					<h6>Find people who have received more than k awards in a single motion picture in the same year</h6>
+				</div>
+				<div class="col">
+					<h6>Place holder</h6>
+				</div>
+			</div>
 
-				// initialize table headers for 'view all movies'
-				echo "<tr>
-				<th class='col-md-2'>user email</th>
-				<th class='col-md-2'>user name</th>
-				<th class='col-md-2'>movie name</th>
-				</tr></thead>";
+			<div class="row">
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Enter country" name="shootingLocation" id="shootingLocation">
+				</div>
+				<div class="col">
+					<button class="btn btn-outline-secondary" type="submit" name="viewMoviesFromLocation" id="button-addon2">View Movies</button>
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Enter zip code" name="zipCode" id="zipCode">
+				</div>
+				<div class="col">
+					<button class="btn btn-outline-secondary" type="submit" name="viewDirectorsFromZipCode" id="button-addon2">View directors</button>
+				</div>
+			</div>
 
-			}else if(isset($_POST['viewAllMovies'])){
 
-				$query = "
-				SELECT MotionPicture.*, Movie.box_office_collection
-				FROM Movie
-				JOIN MotionPicture ON Movie.mpid = MotionPicture.id;
-				";
-
-				// initialize table headers for 'view all movies'
-				echo "<tr>
-				<th class='col-md-2'>id</th>
-				<th class='col-md-2'>name</th>
-				<th class='col-md-2'>rating</th>
-				<th class='col-md-2'>production</th>
-				<th class='col-md-2'>budget</th>
-				<th class='col-md-2'>box office collection</th>
-				</tr></thead>";
-			}else if(isset($_POST['viewAllActors'])){
-				$query = "
-				SELECT Role.role_name, People.*
-				FROM Role
-				JOIN People ON Role.pid = People.id
-				Where Role.role_name = 'actor'
-				";
-
-				// initialize table headers for 'view all movies'
-				echo "<tr>
-				<th class='col-md-2'>role name</th>
-				<th class='col-md-2'>pid</th>
-				<th class='col-md-2'>name</th>
-				<th class='col-md-2'>nationality</th>
-				<th class='col-md-2'>date of birth</th>
-				<th class='col-md-2'>gender</th>
-				</tr></thead>";
-			}
-
-   
-
-			// generic table builder. It will automatically build table data rows irrespective of result
-			class TableRows extends RecursiveIteratorIterator {
-				function __construct($it) {
-					parent::__construct($it, self::LEAVES_ONLY);
-				}
-
-				function current() {
-					return "<td style='text-align:center'>" . parent::current(). "</td>";
-				}
-
-				function beginChildren() {
-					echo "<tr>";
-				}
-
-				function endChildren() {
-					echo "</tr>" . "\n";
-				}
-			}
-
-			// SQL CONNECTIONS
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-			$dbname = "COSI127b";
-
-			try {
-				// We will use PDO to connect to MySQL DB. This part need not be 
-				// replicated if we are having multiple queries. 
-				// initialize connection and set attributes for errors/exceptions
-				$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-				// prepare statement for executions. This part needs to change for every query
-				$stmt = $conn->prepare($query);
-
-				// execute statement
-				$stmt->execute();
-
-				// set the resulting array to associative. 
-				$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-				// for each row that we fetched, use the iterator to build a table row on front-end
-				foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-					echo $v;
-				}
-			}
-			catch(PDOException $e) {
-				echo "Error: " . $e->getMessage();
-			}
-			echo "</table>";
-			// destroy our connection
-			$conn = null;
-    
-    ?>
-
-    </div>
+		</form>
+	</div>
+	<div class="container">	
+		<h3 style="text-align:left">Query Result</h3><br>
+		<?php include 'query.php'; ?>
+	</div>
 </body>
 </html>
